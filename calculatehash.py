2155b79091c01167e524020d54c45c8212696bbf
infinitus <infinitus.internal@gmail.com>
2010-12-22 11:35:00 -0500

    modified:   calculatehash.py

--- calculatehash.py
+++ calculatehash.py
@@ -5,36 +5,36 @@ import hashlib
 import os
 
 
-def readfile(filename):
+def readfile(filename):             #define function to read a file
     f = open(filename)
     content = f.read()
     f.close()
     return content
 
 
-def md5function(content):
+def md5function(content):           #define function to generate md5 hash using hashlib module
     m = hashlib.md5()
     m.update(content)
-    return m.hexdigest()
+    return m.hexdigest() 
 
 
-def main():
-    print "Running.."
-    md5index = {}
-    output = open(r'./output.txt', 'w')
+def main():                         #main function
+    print "Running.."               #start the program
+    md5index = {}                   #create an empty dictionary {key:value}
+    output = open(r'./output.txt', 'w') #create a new output.txt to contain the generated md5 hashes
 
-    for root, dirs, files in os.walk(sys.argv[1]):
-        for eachfile in files:
-            filepath = os.path.join(root, eachfile)
-            content = readfile(filepath)
-            md5sum = md5function(content)
-            md5index[filepath] = md5sum
+    for root, dirs, files in os.walk(sys.argv[1]):  #perform recursive file lookup
+        for eachfile in files:                      #loop through the list of files
+            filepath = os.path.join(root, eachfile) #join the root and the file name to form a filepath
+            content = readfile(filepath)            #call the readfile function for filepath
+            md5sum = md5function(content)           #call md5function to generate md5 hash
+            md5index[filepath] = md5sum             #add a new entry into the md5index 
             
-    for entry in md5index:
-        output.write(entry + "\t" + md5index[entry] + "\n")
+    for entry in md5index:          #for each entry in the md5index
+        output.write(entry + "\t" + md5index[entry] + "\n") #write into the output.txt, with a tab spacing between filename and hash
                 
-    output.close()
-    print "Success!" + "\n" "All filenames and their respective md5sums are indexed in output.txt"
+    output.close()  #close the file
+    print "Success!" + "\n" "All filenames and their respective md5sums are indexed in output.txt"  #print success message when done
 
 
 
